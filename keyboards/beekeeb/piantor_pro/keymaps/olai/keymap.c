f63c2976c29664331fc0634abbdc82e35445bf7d
LilleAila <olai.solsvik@gmail.com>
2023-12-23 11:02:19 +0100

    Updated piantor pro layout

--- keyboards/beekeeb/piantor_pro/keymaps/olai/keymap.c
+++ keyboards/beekeeb/piantor_pro/keymaps/olai/keymap.c
@@ -2,10 +2,13 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #include QMK_KEYBOARD_H
-#include "keymap_norwegian.h"
+// #include "keymap_norwegian.h"
+#include "keymap_mac_norwegian.h"
 
+// Docs
 // https://github.com/qmk/qmk_firmware/blob/master/docs/keycodes_basic.md
-// https://github.com/qmk/qmk_firmware/blob/master/quantum/keymap_extras/keymap_norwegian.h
+// https://github.com/qmk/qmk_firmware/blob/master/docs/feature_advanced_keycodes.md
+// https://github.com/qmk/qmk_firmware/blob/master/docs/feature_layers.md
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // Most often, only master half needs lto be flashed after first flash
@@ -19,31 +22,39 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             KC_ESC,  KC_Q,        KC_W,        KC_F,        KC_P,        KC_B, /**/ KC_J, KC_L,        KC_U,        KC_Y,        NO_OSTR,     NO_AE,
             XXXXXXX, CTL_T(KC_A), OPT_T(KC_R), CMD_T(KC_S), SFT_T(KC_T), KC_G, /**/ KC_M, SFT_T(KC_N), CMD_T(KC_E), OPT_T(KC_I), CTL_T(KC_O), NO_ARNG,
             KC_LSFT, KC_Z,        KC_X,        KC_C,        KC_D,        KC_V, /**/ KC_K, KC_H,        KC_COMM,     KC_DOT,      KC_MINUS,    XXXXXXX,
-            // Thumb keys (align separately)
-            KC_TAB, KC_BSPC, LT(2, KC_ENT), /**/ LT(1, KC_SPC), KC_DEL, KC_ESC
+            //
+            KC_TAB, LT(2, KC_ENT), KC_DEL, /**/ KC_BSPC, LT(1, KC_SPC), KC_ESC
             ),
 
     [1] = LAYOUT_split_3x6_3( // Num and nav
-            _______, NO_ASTR, KC_7, KC_8, KC_9, NO_PLUS, /**/ _______, _______, _______, _______, _______,  _______,
-            _______, KC_0,    KC_4, KC_5, KC_6, NO_EQL,  /**/ _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, _______,
-            _______, NO_SLSH, KC_1, KC_2, KC_3, NO_MINS, /**/ _______, _______, _______, _______, _______,  _______,
+            NO_NEQ,  NO_ASTR, KC_7, KC_8, KC_9, NO_PLUS, /**/ _______, _______, KC_PGUP, _______, _______,  _______,
+            NO_APRX, KC_0,    KC_4, KC_5, KC_6, NO_EQL,  /**/ _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, _______,
+            NO_NEQ,  NO_SLSH, KC_1, KC_2, KC_3, NO_MINS, /**/ _______, _______, KC_PGDN, _______, _______,  _______,
             //
             _______, MO(3), _______, /**/ _______, _______, _______
             ),
 
     [2] = LAYOUT_split_3x6_3( // Sym
-            _______, _______, _______, _______, _______, _______, /**/ _______, _______, _______, _______, _______, _______,
-            _______, _______, _______, _______, _______, _______, /**/ _______, _______, _______, _______, _______, _______,
-            _______, _______, _______, _______, _______, _______, /**/ _______, _______, _______, _______, _______, _______,
+            NO_PM,   NO_PERC, NO_GRV,  NO_TILD, NO_LEQ,  NO_GEQ,  /**/ NO_LPRN, NO_RPRN, NO_EXLM, NO_COLN, NO_DLR,  _______,
+            NO_OMEG, NO_AMPR, NO_ACUT, NO_BTCK, NO_LABK, NO_RABK, /**/ NO_LBRC, NO_RBRC, NO_DQUO, NO_QUOT, NO_SLSH, _______,
+            NO_CIRC, NO_SQRT, NO_AT,   NO_UNDS, NO_PIPE, NO_HASH, /**/ NO_LCBR, NO_RCBR, NO_QUES, NO_SCLN, NO_BSLS, _______,
             //
             _______, _______, _______, /**/ _______, MO(3), _______
             ),
 
-    [3] = LAYOUT_split_3x6_3( // Other, for example toggle to gaming layer. Gaming layer will use the same thumb keys with the same layers, but slightly modified qwerty
-            QK_BOOT, _______, _______, _______, _______, _______, /**/ _______, _______, _______, _______, _______, _______,
-            _______, _______, _______, _______, _______, _______, /**/ _______, _______, _______, _______, _______, _______,
-            _______, _______, _______, _______, _______, _______, /**/ _______, _______, _______, _______, _______, _______,
+    [3] = LAYOUT_split_3x6_3( // Function
+            QK_BOOT, DF(0),   DF(4),   _______, _______, _______, /**/ KC_MPRV, KC_MNXT, KC_VOLU, KC_BRIU, _______, KC_SLEP,
+            _______, _______, _______, _______, _______, _______, /**/ KC_MSTP, KC_MPLY, KC_VOLD, KC_BRID, _______, _______,
+            _______, _______, _______, _______, _______, _______, /**/ _______, _______, KC_MUTE, _______, _______, _______,
             //
             _______, _______, _______, /**/ _______, _______, _______
+            ),
+
+    [4] = LAYOUT_split_3x6_3( // Gaming (almost qwerty)
+            _______, _______, KC_Q,    KC_W,    KC_E,    _______, /**/ _______, _______, _______, _______, _______, _______,
+            _______, _______, KC_A,    KC_S,    KC_D,    _______, /**/ _______, _______, _______, _______, _______, _______,
+            _______, _______, _______, _______, _______, _______, /**/ _______, _______, _______, _______, _______, _______,
+            //
+            KC_TAB, LT(2, KC_ENT), KC_DEL, /**/ KC_BSPC, LT(1, KC_SPC), KC_ESC
             )
 };
