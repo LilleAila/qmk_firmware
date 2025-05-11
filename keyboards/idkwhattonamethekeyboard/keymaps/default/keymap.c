1912a144ff7070ff45419412472d9e523a107da9
LilleAila <olai.solsvik@gmail.com>
2025-05-11 18:44:54 +0200

    colemak

--- keyboards/idkwhattonamethekeyboard/keymaps/default/keymap.c
+++ keyboards/idkwhattonamethekeyboard/keymaps/default/keymap.c
@@ -6,6 +6,11 @@
 
 enum layers {
     _QWERTY,
+    _COLEMAK, // colemak dh
+};
+
+enum {
+    TD_LAYER_CYCLE
 };
 
 enum combos {
@@ -24,7 +29,7 @@ enum combos {
     Q_LSFT,
     Q_RSFT,
     Q_END,
-    // Colemak (dh) combos
+    // Colemak combos
     C_START,
     C_END,
 };
@@ -69,10 +74,30 @@ bool combo_should_trigger(uint16_t combo_index, combo_t *combo, uint16_t keycode
     return true;
 }
 
+void layer_cycle(tap_dance_state_t *state, void *user_data) {
+    switch (state->count) {
+        case 1:
+            layer_move(_QWERTY);
+            break;
+        case 2:
+            layer_move(_COLEMAK);
+            break;
+    }
+}
+
+tap_dance_action_t tap_dance_actions[] = {
+    [TD_LAYER_CYCLE] = ACTION_TAP_DANCE_FN(layer_cycle),
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [_QWERTY] = LAYOUT_2x12_3(
-        XXXXXXX, KC_Q, KC_W, KC_E, KC_R, KC_T /**/, KC_Y, KC_U, KC_I,    KC_O,   KC_P,    NO_AA,
-        XXXXXXX, KC_Z, KC_X, KC_C, KC_V, KC_B /**/, KC_N, KC_M, KC_COMM, KC_DOT, NO_MINS, NO_AE,
-        QK_BOOT, QK_BOOT, /**/ QK_BOOT, QK_BOOT
+    [_QWERTY] = LAYOUT_2x12_4(
+        TD(TD_LAYER_CYCLE), KC_Q, KC_W, KC_E, KC_R, KC_T, /**/ KC_Y, KC_U, KC_I,    KC_O,   KC_P,    NO_AA,
+        QK_BOOT,            KC_Z, KC_X, KC_C, KC_V, KC_B, /**/ KC_N, KC_M, KC_COMM, KC_DOT, NO_MINS, NO_AE,
+        KC_ENT, KC_SPC, /**/ KC_SPC, KC_BSPC
+    ),
+    [_COLEMAK] = LAYOUT_2x12_4(
+        TD(TD_LAYER_CYCLE), KC_Q, KC_W, KC_F, KC_P, KC_B, /**/ KC_J, KC_L, KC_U,    KC_Y,   NO_OE,   NO_AE,
+        QK_BOOT,            KC_Z, KC_X, KC_C, KC_D, KC_V, /**/ KC_K, KC_H, KC_COMM, KC_DOT, NO_MINS, XXXXXXX,
+        KC_ENT, KC_SPC, /**/ KC_SPC, KC_BSPC
     )
 };
